c87cce6ab4b9f5b125be32e9308a2fb96bf41c3b
Evin O'Shea <eco2000@wildcats.unh.edu>
2018-09-26 14:25:23 -0400

    Fixed unittests for routes/links -- added route for signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "static_pages#home"
+	get  '/signup',  to: 'users#new'
   resources :accomplishments
 	resources :static_pages
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
