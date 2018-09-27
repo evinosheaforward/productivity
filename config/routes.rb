dee791d6aff979828a57f1ebfc3f661fd06a980f
Evin O'Shea <eco2000@wildcats.unh.edu>
2018-09-27 17:59:45 -0400

    Implement basic login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  root "static_pages#home"
-	get  '/signup',  to: 'users#new'
+	root   'static_pages#home'
+	# get '/help', to: 'static_pages#help'
+	get    '/signup',  to: 'users#new'
+	get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
   resources :accomplishments
 	resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -5,6 +5,6 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
-    assert_select "a[href=?]", "/", count: 1
+    assert_select "a[href=?]", "/", count: 2
   end
 end
