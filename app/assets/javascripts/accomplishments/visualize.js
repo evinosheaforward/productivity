9eb85dead85ba8458779c127b005d1a55a5a0366
Evin O'Shea <eco2000@wildcats.unh.edu>
2018-10-09 16:12:08 -0400

    Add a second plot to Stats page (visualizations)

--- app/assets/javascripts/accomplishments/visualize.js
+++ app/assets/javascripts/accomplishments/visualize.js
@@ -6,7 +6,7 @@ var loadData = function(){
                   dataType: 'json',
                   success: function(data){
 										console.log("Making the plot!");
-                    drawBarPlot(data);
+                    drawScatterPlot(data);
                   },
                   failure: function(result){
                     error();
@@ -15,22 +15,31 @@ var loadData = function(){
               };
 
 function error() {
-    console.log("Something went wrong!");
+  console.log("Ajax GET request failed for accomplishments!");
 }
 
+// Handle datetime parsing for ruby dates
+function parseTime(s) {
+	var p = d3.timeParse("%Y-%m-%dT%H:%M");
+	return p(s.substring(0, 16));
+}
 
-function drawBarPlot(data){
+function drawScatterPlot(data){
+	var radius = 7.5;
 	// set the dimensions and margins of the graph
-	var margin = {top: 20, right: 20, bottom: 30, left: 50};
+	var margin = {top: 20, right: 20, bottom: 50, left: 50};
 	var width = 768 - margin.left - margin.right;
   var height = 400 - margin.top - margin.bottom;
 
-	var prodMax = 10 + d3.max(data, function(d) {
+	var prodMax = 5 + d3.max(data, function(d) {
 		return (d.timeProductive/d.timeWorking)*100;
 	});
-	var workMax = d3.max(data, function(d) { return d.timeWorking; });
+	var workMax = d3.max(data, function(d) {
+		return +d.timeWorking;
+	});
 
-	var svg = d3.select("#plot")
+	// Productivity vs time working on task
+	var svg = d3.select("#prodWorking")
 		.attr("width", width + margin.left + margin.right)
 		.attr("height", height + margin.top + margin.bottom);
 
@@ -43,7 +52,6 @@ function drawBarPlot(data){
 
 	var xAxis = d3.axisBottom().scale(xScale);
 	var yAxis = d3.axisLeft().scale(yScale);
-
 
 	svg.append("g")
 		.call(xAxis)
@@ -68,7 +76,7 @@ function drawBarPlot(data){
 		.text("Productivity (%)");
 
 	svg.append("g")
-		.selectAll("circle")
+		.selectAll(".dot")
 		.data(data)
 	  .enter().append("circle")
       .attr("class", "dot")
@@ -78,61 +86,65 @@ function drawBarPlot(data){
 			.attr("cy", function(d) {
 				return yScale(100 * d.timeProductive/d.timeWorking);
 			})
-			.attr("r", 10)
+			.attr("r", radius)
 			.style("fill", "blue")
 
-			// .data(data)
-			// // .enter()
-			// .append("circle")
-			// // .selectAll(".dot")
-			// // .data(data)
-	  	// // .enter()
-			// // .append("circle")
-      // // .attr("class", "dot")
-			// .attr("fill", "blue");
-			// .attr("r", 10)
-		// 	// Add the scatterplot
-	 // svg.selectAll("dot")
-	 //      .data(data)
-	 //    .enter().append("circle")
-	 //      .attr("r", 20)
-	 //      .attr("cx", function(d) { return x(d.timeWorking/100); })
-	 //      .attr("cy", function(d) { return y(d.timeProductive/100); });
-	//
-	// svg.append("g")
-	//       .attr("class", "y axis")
-	//       .call(yAxis)
-	//     	.append("text")
-	//       .attr("class", "label")
-	//       .attr("transform", "rotate(-90)")
-	//       .attr("y", 6)
-	//       .attr("dy", ".71em")
-	//       .style("text-anchor", "end")
-	//       .text("Sepal Length (cm)");
-	//
-	// svg.append("g")
-  //      .attr("class", "x axis")
-  //      .attr("transform", "translate(0," + height + ")")
-  //      .call(xAxis)
-  //    	 .append("text")
-  //      .attr("class", "label")
-  //      .attr("x", width)
-  //      .attr("y", -6)
-  //      .style("text-anchor", "end")
-  //      .text("Sepal Width (cm)");
-	 //
-	 // svg.append("path")
-   //     .data([data])
-   //     .attr("class", "line")
-   //     .attr("d", valueline);
-	 //
-		// 	 // Add the scatterplot
-   // svg.selectAll("dot")
-   //     .data(data)
-   //   .enter().append("circle")
-   //     .attr("r", 5)
-   //     .attr("cx", function(d) { return x(d.timeWorking); })
-   //     .attr("cy", function(d) { return y(d.timeProdutive); });
+	// Productivity over time plot
+	var prodTime = d3.select("#timeProd")
+		.attr("width", width + margin.left + margin.right)
+		.attr("height", height + margin.top + margin.bottom);
 
+	var timeMin = d3.min(data, function(d) {
+		return parseTime(d.date);
+	});
+	var timeMax = d3.max(data, function(d) {
+		return parseTime(d.date);
+	});
 
+	xScale = d3.scaleTime()
+		.domain([timeMin, timeMax])
+		.range([0, width]);
+	yScale = d3.scaleLinear()
+		.domain([prodMax, 0])
+		.range([0, height]);
+
+	xAxis = d3.axisBottom().scale(xScale);
+	yAxis = d3.axisLeft().scale(yScale);
+
+	prodTime.append("g")
+    .attr("class", "axis")
+    .attr("transform", "translate(60," + height + ")")
+    .call(xAxis
+            .tickFormat(d3.timeFormat("%m-%d-%y")))
+    .selectAll("text")
+      .style("text-anchor", "end")
+      .attr("dx", "-.8em")
+      .attr("dy", ".15em")
+      .attr("transform", "rotate(-65)")
+			.attr("font-size", "1.2em");
+
+	prodTime.append("g")
+		.call(yAxis)
+		.attr("transform", "translate(60, 0)")
+		.attr("font-size", "1.2em");
+
+	prodTime.append("text")
+	  .attr("fill", "#000")
+		.attr("transform", "translate(20, 250) rotate(-90)")
+		.attr("font-size", "1.2em")
+		.text("Productivity (%)");
+
+	prodTime.append("g")
+		.selectAll(".prog")
+		.data(data)
+	  .enter().append("circle")
+			.attr("class", "prog")
+			.attr("cx", function(d) {
+				return 60 + xScale(parseTime(d.date));
+			})
+			.attr("cy", function(d) {
+				return yScale(100 * d.timeProductive/d.timeWorking);
+			})
+			.attr("r", radius)
+			.style("fill", "blue");
 }
